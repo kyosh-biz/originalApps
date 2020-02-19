271ec5ff12abc62e3eec8fa2a1fbfd2558ec591f
kyosh-biz <60495636+kyosh-biz@users.noreply.github.com>
2020-02-19 14:37:16 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,58 @@
+'use strict';
+
+{
+    const num = 10;
+
+    const container = document.getElementById('container');
+
+    for(let i = 0; i < num; i++){
+        const div = document.createElement('div');
+        div.classList.add('tbox');
+        
+        const img = document.createElement('img');
+        img.setAttribute("src","img/宝箱.png");
+        img.classList.add('img');
+
+        const item = document.createElement('span');
+        item.classList.add('item');
+        // item.textContent = "click";
+
+        img.addEventListener('click',() =>{
+            const n = Math.random();
+        if(n < 0.07) {
+            img.style.display = "none";
+            const results = ['天空のつるぎ','天空のよろい','天空の盾','ルーンバックラー' , 'hogehogehogehogehogeのやり'];
+            item.textContent = results[Math.floor(Math.random() * results.length)];
+            div.classList.add('star5');
+        }else if(n < 0.3) {
+            img.style.display = "none";
+            item.textContent = "☆4"
+            div.classList.add('star4');
+        }else {
+            img.style.display = "none";
+            item.textContent = "☆3"
+            div.classList.add('star3');
+        }
+        });
+        container.appendChild(div);
+        div.appendChild(img);
+        div.appendChild(item);
+    }
+
+    // const box = document.getElementById('box');
+    // const item = document.getElementById('item');
+    // box.addEventListener('click' , () => {
+    //     const n = Math.random();
+    //     if(n < 0.07) {
+    //         const results = ['天空のつるぎ','天空のよろい','天空の盾','ルーンバックラー' , 'hogehogehogehogehogeのやり'];
+    //         item.textContent = results[Math.floor(Math.random() * results.length)];
+    //         box.classList.add('star5');
+    //     }else if(n < 0.3) {
+    //         item.textContent = "☆4"
+    //         box.classList.add('star4');
+    //     }else {
+    //         item.textContent = "☆3"
+    //         box.classList.add('star3');
+    //     }
+    // });
+}
